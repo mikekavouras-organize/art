268195e52c2d0c2c6f8874e637925019707713d0
Mike Kavouras <mikekavouras@github.com>
2019-12-30 15:10:08 -0500

    Admin namespace and admin#user routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   root to: "welcome#index"
+
+  namespace :admin do
+    root to: "welcome#index"
+    resources :users, only: [:new]
+
+    post 'login', to: 'sessions#create'
+  end
 end
