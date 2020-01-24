ec8be3a91d247f72542fc5a92ff8b0396d6d3bcb
Mike Kavouras <mikekavouras@github.com>
2020-01-23 20:19:28 -0500

    Create pieces_controller.rb

---
+++ app/controllers/admin/pieces_controller.rb
@@ -0,0 +1,47 @@
+module Admin
+  class PiecesController < ApplicationController
+    def show
+      @piece = Piece.find(params[:id])
+    end
+
+    def new
+      @piece = Piece.new
+    end
+
+    def create
+      category.pieces.create!(piece_params)
+      redirect_to admin_category_path(category)
+    rescue ActiveRecord::RecoredInvalid => e
+      flash[:error] = e.message
+      redirect_to admin_category_path(category)
+    end
+
+    def edit
+      @piece = Piece.find(params[:id])
+    end
+
+    def update
+      piece.update!(piece_params)
+      redirect_to admin_category_path(category)
+    rescue ActiveRecord::RecoredInvalid => e
+      flash[:error] = e.message
+      redirect_to admin_category_path(category)
+    end
+
+    private
+
+    def category
+      @category ||= Category.find_by(name: params[:category_name])
+    end
+    helper_method :category
+
+    def piece
+      @piece ||= Piece.find(params[:id])
+    end
+
+    def piece_params
+      params.require(:piece).permit(:title, :description)
+    end
+  end
+end
+
