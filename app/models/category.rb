88202675a3e9d425dbd661e3ea6de21fa6c86914
Mike Kavouras <mikekavouras@github.com>
2020-01-23 20:19:22 -0500

    Space

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,14 +1,14 @@
 class Category < ApplicationRecord
-    has_many :collections
-    has_many :pieces
+  has_many :collections
+  has_many :pieces
 
-    before_save :downcase_name
+  before_save :downcase_name
 
-    def to_param
-      name
-    end
+  def to_param
+    name
+  end
 
-    def downcase_name
-      self.name.downcase!
-    end
+  def downcase_name
+    self.name.downcase!
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: "welcome#index"
     resources :sessions, only: [:new, :create]
+    resources :categories, except: [:index], param: :name do
+      resources :pieces
+    end
   end
-
 end
