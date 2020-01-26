31c9d4e627a41b9724093b5f89d8da5035631846
Mike Kavouras <mikekavouras@github.com>
2020-01-26 14:09:59 -0500

    remove media_positions column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_26_145904) do
+ActiveRecord::Schema.define(version: 2020_01_26_190908) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,16 @@ ActiveRecord::Schema.define(version: 2020_01_26_145904) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "assets", force: :cascade do |t|
+    t.integer "piece_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "asset_file_name"
+    t.string "asset_content_type"
+    t.bigint "asset_file_size"
+    t.datetime "asset_updated_at"
+  end
+
   create_table "categories", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -57,7 +67,6 @@ ActiveRecord::Schema.define(version: 2020_01_26_145904) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "category_id"
-    t.integer "media_positions", default: [], array: true
     t.index ["category_id"], name: "index_pieces_on_category_id"
   end
 
