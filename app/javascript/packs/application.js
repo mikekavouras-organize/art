3f7c3cb6a98f9e6bf8ceb3f74bb8249aac5cd456
Mike Kavouras <mikekavouras@github.com>
2020-01-25 20:32:00 -0500

    preload some images

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,29 +1,42 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-
 import {observe} from 'selector-observer'
+
+const preloadImage = (image, onLoad) => {
+  let i = new Image()
+  i.onload = onLoad
+  i.src = image.src
+}
+
+const preloadImages = (images, onLoad) => {
+  let count = 0
+  for (const image of images) {
+    preloadImage(image, () => {
+      console.log('loaded')
+      count++
+      if (count == images.length) { onLoad() }
+    })
+  }
+}
 
 observe('.js-file-input', {
   add(input) {
     input.addEventListener('change', async event => {
       const formData = new FormData(input.form)
-      // const [file] = event.target.files
       const method = input.form.querySelector('[name=_method]').value
       const response = await fetch(input.form.action, {
         method: method.toUpperCase(),
         headers: { "X-Requested-With": "XMLHttpRequest" },
         body: formData
       })
-      const html = await response.text()
 
+      const html = await response.text()
       const div = document.createElement('div')
       div.classList.add('js-media-form')
       div.innerHTML = html.trim()
-      document.querySelector('.js-media-form').replaceWith(div)
 
-      input.form.reset()
+      const images = div.querySelectorAll('img')
+      preloadImages(images, () => {
+        document.querySelector('.js-media-form').replaceWith(div)
+      })
     })
   }
 })
