a1666d0b318245a3d9a6f98321439e1559564afc
Mike Kavouras <mikekavouras@github.com>
2020-01-26 10:03:26 -0500

    Add index on categories.name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_26_043610) do
+ActiveRecord::Schema.define(version: 2020_01_26_145904) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,9 +48,10 @@ ActiveRecord::Schema.define(version: 2020_01_26_043610) do
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.index ["name"], name: "index_categories_on_name"
   end
 
   create_table "collections", force: :cascade do |t|
