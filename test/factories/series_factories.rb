c641292a2a301ecc56d1ea9578a3b228ddbc70d0
Mike Kavouras <mikekavouras@github.com>
2020-03-26 22:52:50 -0400

    series factory

--- test/factories/series_factories.rb
+++ test/factories/series_factories.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
-  factory :piece do
+  factory :series do
     title { Faker::Creature::Animal.name }
     description { Faker::Lorem.paragraph(sentence_count: 3) }
     category { create(:category) }
