5510880d586c4d755e9ce97fb071d1ab3580eb10
Mike Kavouras <mikekavouras@github.com>
2020-03-27 00:33:24 -0400

    card and details

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,20 +2,38 @@ import {observe} from 'selector-observer'
 import {on} from 'delegated-events'
 
 function doTheThing(assetId) {
-  const galleryImage = document.querySelector('.js-gallery-image')
   const template = document.querySelector(`.js-asset-${assetId}`)
-  const image = template.content.querySelector('img')
-  const seriesTitle = template.content.querySelector('.js-title').textContent
-  const seriesDescription = template.content.querySelector('.js-description').textContent
-  document.querySelector('.js-gallery-image img').src = image.src
-  document.querySelector('.js-series-title').textContent = seriesTitle
-  document.querySelector('.js-series-description').textContent = seriesDescription
+  const content = template.content.cloneNode(true)
+  const newPiece = content.querySelector('.js-piece')
+  const visiblePiece = document.querySelector('.js-visible-piece')
+  visiblePiece.querySelector('.js-visible-piece-content').replaceWith(newPiece)
+
+  newPiece.classList.remove('js-piece')
+  newPiece.classList.add('js-visible-piece-content')
+
+  // process metadata
+  const metadata = newPiece.querySelector('.js-piece-metadata')
+  const width = metadata.getAttribute('data-width')
+  const height = metadata.getAttribute('data-height')
+  if (width === '' || height === '') return
+
+  const assetContainer = newPiece.querySelector('.js-asset-container')
+  const widthInt = parseInt(width)
+  const heightInt = parseInt(height)
+  const maxHeight = 640
+  const maxWidth = 640
+  const isVertical = widthInt < heightInt
+  const ratio = isVertical ? maxHeight / heightInt :  maxWidth / widthInt
+  const newHeight = heightInt * ratio
+  const newWidth = widthInt * ratio
+  assetContainer.setAttribute('style', `width:${newWidth}px;height:${newHeight}px`)
 }
 
 on('click', '.js-navigate-next', e => {
   e.preventDefault()
   currentAssetIdx++
   const assetId = assetIds[currentAssetIdx % assetIds.length]
+  console.log(assetId)
   doTheThing(assetId)
 })
 
@@ -25,3 +43,6 @@ on('click', '.js-navigate-previous', ({currentTarget}) => {
   const assetId = assetIds[currentAssetIdx % assetIds.length]
   doTheThing(assetId)
 })
+
+while (!assetIds) {}
+doTheThing(assetIds[0])
