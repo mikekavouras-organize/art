08a9f8f3951e10513330e1d1eced05267dac44de
Mike Kavouras <mikekavouras@github.com>
2020-04-10 17:38:01 -0400

    Mobile looks so good thanks @jayschaul

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -47,18 +47,10 @@ on('click', '.js-menu-toggle', function() {
   const menu = document.querySelector('.js-menu')
   if (menu.classList.contains('active')) {
     menu.classList.remove('active')
-    const overlay = document.querySelectorAll('.js-overlay')
-    for (const o of overlay) {
-      o.classList.remove('visible')
-      setTimeout(function() { o.remove() }, 400)
-    }
+    document.body.style.overflow = 'auto'
   } else {
     menu.classList.add('active')
-    const overlay = document.createElement('div')
-    overlay.classList.add('overlay')
-    overlay.classList.add('js-overlay')
-    document.body.appendChild(overlay)
-    setTimeout(function() { overlay.classList.add('visible') }, 10);
+    document.body.style.overflow = 'hidden'
   }
 })
 
