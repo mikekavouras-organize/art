70a1ca8899660922746dd171df830ab717a35933
Mike Kavouras <kavourasm@gmail.com>
2023-07-25 10:08:44 -0400

    Safer resume check

--- app/controllers/admin/welcome_controller.rb
+++ app/controllers/admin/welcome_controller.rb
@@ -9,15 +9,12 @@ module Admin
         categories: Category.all.order(position: :asc),
         info: ArtistInfo.last || ArtistInfo.new,
         resume: Resume.last,
+        resume_preview: resume_preview
       }
     end
 
     def update_category_positions
       category_ids = params.delete("positions").split(",")
-
-      puts "*" * 80
-      puts category_ids
-      puts "*" * 80
 
       case_string = "position = CASE id "
       category_ids.each.with_index do |id, index|
@@ -26,6 +23,15 @@ module Admin
       case_string += "END"
 
       Category.where(id: category_ids).update_all(case_string)
+    end
+
+    def resume
+      return @resumen if defined?(@resume)
+      @resume = Resume.last
+    end
+
+    def resume_preview
+      resume&.attachment.preview(resize_to_limit: [640, 640]) rescue nil
     end
   end
 end
